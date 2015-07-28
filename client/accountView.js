abbd852822dcdcffa4b3b0a928a97dc6f66901d2
padraigobrien <padraig@padraigobrien.com>
2015-07-28 22:29:13 +0100

    updating signup page

--- client/accountView.js
+++ client/accountView.js
@@ -1,9 +1,7 @@
 Template.login.events({
     'click .circle':function(event){
-        Meteor.loginWithFacebook({ requestPermissions: ['email', 'public_profile']}, function(err){
-            console.log(Meteor.user());
-            console.log(Meteor.user().profile.name);
-            console.log(Meteor.user().profile.picture);
+        Meteor.loginWithFacebook({ requestPermissions: ['email', 'public_profile']}, function(err)
+        {
             Router.go('/home');
         });
     }
